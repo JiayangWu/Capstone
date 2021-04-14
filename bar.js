ee696b1c199ba5b8274faa997a3f719b3b0aedec
JiayangWu <530081999@qq.com>
2021-04-14 11:37:15 -0400

    modify css

--- bar.js
+++ bar.js
@@ -21,7 +21,7 @@ var x_axis = d3.axisBottom()
 
 // # https://colourco.de
 let color3 = d3.scaleOrdinal()
-    .range(d3.quantize(d3.interpolateHcl("#9AA6DD", "#ded79b"), 50));
+    .range(d3.quantize(d3.interpolateHcl("#9AA6DD", "#ded79b"), 51));
 
 let y_labels = svg3.append("g");
 
@@ -37,6 +37,7 @@ var y3 = d3.scaleLinear()
 
 let y3_desc = svg3.append("text");
 let x3_desc = svg3.append("text");
+let title3 = svg3.append("text");
 
 let tooltip3 = d3.select("body")
     .append("div")
@@ -46,23 +47,25 @@ let tooltip3 = d3.select("body")
 let x3_publishers = svg3.append("g");
 
 let mouseover3 = function (d) {
-    let color_span = `<span style="color: ${color3(parseInt(d.Tweets_Cnt))}; font-size: 18px; font-weight:bold;">`;
+    let color_span = `<span style="color: ${color3(d.State_Code)}; font-size: 18px; font-weight:bold;">`;
     let html = `${d.State_Code}<br/>
                 Tweets Count:<br/>
                 ${color_span}${d.Tweets_Cnt}</span>`;
-
+    // console.log(color3(d.State_Code));
+    // console.log(parseInt(d.Tweets_Cnt));
+    // console.log(color3(d.State_Code));
     // Show the tooltip and set the position relative to the event X and Y location
     tooltip3.html(html)
         .style("left", `${(d3.event.pageX) + 30}px`)
         .style("top", `${(d3.event.pageY) - 100}px`)
-        .style("box-shadow", `3px 3px 7px ${color3(parseInt(d.Tweets_Cnt))}`)
+        .style("box-shadow", `3px 3px 7px ${color3(d.State_Code)}`)
         .style("background-color", "#ffffff")
         .transition()
         .duration(400)
         .style("opacity", 0.9)
 
     svg3.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
-        return darkenColor(color3(parseInt(d.Tweets_Cnt)), 0.5);
+        return darkenColor(color3(d.State_Code), 0.5);
     });
 }
 
@@ -72,7 +75,7 @@ let mouseout3 = function (d) {
         .duration(300)
         .style("opacity", 0);
     svg3.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
-        return color3(parseInt(d.Tweets_Cnt));
+        return color3(d.State_Code);
     });
 };
 
@@ -84,16 +87,19 @@ d3.csv("./data/full_dataset.csv").then(function (data3) {
     y3.domain([0, d3.max(data3, function (d) { return parseInt(d.Tweets_Cnt) })]);
 
     // y-label
-    y3_desc.attr("transform", `translate(-50, ${(height - margin.top - margin.bottom) / 2 + 80})rotate(-90)`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
+    y3_desc.attr("transform", `translate(-60, ${(height - margin.top - margin.bottom) / 2 + 80})rotate(-90)`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
         .style("text-anchor", "middle")
         .style("font-size", 18)
         .text("Tweets Counts in Each State");
 
     // Add chart title
-    // x3_desc.attr("transform", `translate(${(width - margin.left - margin.right) / 2}, ${-25})`)      // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
-    //     .style("text-anchor", "middle")
-    //     .style("font-size", 18)
-    //     .text("Tweets Count VS State Code");
+    x3_desc.attr("transform", `translate(500, -25)`)      // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
+        .style("text-anchor", "middle")
+        .style("font-size", 18)
+        .text("Tweets Count VS State Code");
+
+    title3.style("font-size", 18)
+        .text("State Code").attr("transform", `translate(500, 550)`);
 
     // x-label
     x3_publishers
@@ -101,7 +107,8 @@ d3.csv("./data/full_dataset.csv").then(function (data3) {
         .call(x_axis)
         // .selectAll("text")
         // 
-        .style("text-anchor", "end").attr("transform", "translate(0,500)");
+        .style("text-anchor", "end")
+        .attr("transform", "translate(0,500)");
 
 
     y_labels.call(d3.axisLeft(y3).tickSizeOuter(0))
@@ -116,7 +123,7 @@ d3.csv("./data/full_dataset.csv").then(function (data3) {
         .attr("y", function (d) { return y3(parseInt(d.Tweets_Cnt)); })
         .attr("width", x3.bandwidth())
         .attr("height", function (d) { return y3(0) - y3(parseInt(d.Tweets_Cnt)); })
-        .attr("fill", function (d) { return color3(parseInt(d.Tweets_Cnt)); })
+        .attr("fill", function (d) { return color3(d.State_Code); })
         .on("mouseover", mouseover3)
         .on("mouseout", mouseout3)
         .attr("id", function (d) { return `rect-${d.State_Code}` });
