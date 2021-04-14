1088718a0f3e3df56401106b4f3edc31bcf9d190
JiayangWu <530081999@qq.com>
2021-04-14 17:45:43 -0400

    clear git

---
+++ bar.js
@@ -0,0 +1,141 @@
+
+
+var svg5 = d3.select("#graph5")
+    .append("svg")
+    .attr("width", 1400)
+    .attr("height", 500 * 2)
+    // .attr("left", 500)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+let countRef5 = svg5.append("g");
+
+
+var x5 = d3.scaleBand()
+    .range([0, 1100])
+    // .attr("transform", "translate(0,500)")
+    .padding(0.5);
+
+var x_axis = d3.axisBottom()
+    .scale(x5);
+
+// # https://colourco.de
+let color5 = d3.scaleOrdinal()
+    .range(d3.quantize(d3.interpolateHcl("#9AA6DD", "#ded79b"), 51));
+
+let y5_labels = svg5.append("g");
+
+var y5 = d3.scaleLinear()
+    .range([500, 0])
+
+// svg5.append("text")
+//     .attr("transform", `translate(${(width - margin.left - margin.right) / 2},
+// ${(height - margin.top - margin.bottom) + 60})`)       // HINT: Place this at the bottom middle edge of the graph - use translate(x, y) that we discussed earlier
+//     .style("text-anchor", "middle")
+//     .style("font-size", 18)
+//     .text("Publisher");
+
+let y5_desc = svg5.append("text");
+let title5 = svg5.append("text");
+let x5_desc = svg5.append("text");
+
+let tooltip5 = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip3")
+    .style("opacity", 0);
+
+let x5_axis = svg5.append("g");
+
+let mouseover5 = function (d) {
+    let color_span = `<span style="color: ${darkenColor(color5(d.State_Code), 1.1)}; font-size: 18px; font-weight:bold;">`;
+    let html = `${d.State_Code}<br/>
+                Tweets Count:<br/>
+                ${color_span}${d.Tweets_Cnt}</span>`;
+    // console.log(color5(d.State_Code));
+    // console.log(parseInt(d.Tweets_Cnt));
+    // console.log(color5(d.State_Code));
+    // Show the tooltip and set the position relative to the event X and Y location
+    tooltip5.html(html)
+        .style("left", `${(d3.event.pageX) + 50}px`)
+        .style("top", `${(d3.event.pageY) - 100}px`)
+        .style("box-shadow", `5px 5px 7px ${color5(d.State_Code)}`)
+        .style("background-color", "#ffffff")
+        .transition()
+        .duration(400)
+        .style("opacity", 0.9)
+
+    svg5.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
+        return darkenColor(color5(d.State_Code), 0.5);
+    });
+}
+
+let mouseout5 = function (d) {
+    // Set opacity back to 0 to hide
+    tooltip5.transition()
+        .duration(500)
+        .style("opacity", 0);
+    svg5.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
+        return color5(d.State_Code);
+    });
+};
+
+d3.csv("./data/full_dataset.csv").then(function (data5) {
+    svg5.selectAll("rect").remove();
+
+    x5.domain(data5.map(function (d) { return d.State_Code }));
+    y5.domain([0, d3.max(data5, function (d) { return parseInt(d.Tweets_Cnt) })]);
+
+    // Add y-axis description
+    y5_desc.attr("transform", `translate(-60, ${(height - margin.top - margin.bottom) / 2 + 80})rotate(-90)`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
+        .style("text-anchor", "middle")
+        .style("font-size", 18)
+        .text("Tweets Counts in Each State");
+
+    // Add chart title
+    title5.attr("transform", `translate(500, -25)`)      // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
+        .style("text-anchor", "middle")
+        .style("font-size", 18)
+        .text("Tweets Count VS State Code");
+
+    // Add x-axis description
+    x5_desc.style("font-size", 18)
+        .text("State Code").attr("transform", `translate(500, 550)`);
+
+    // Create x-axis
+    x5_axis
+        .call(x_axis)
+        .style("text-anchor", "end")
+        .attr("transform", "translate(0,500)");
+
+
+    y5_labels.call(d3.axisLeft(y5).tickSizeOuter(0))
+        .attr("transform", "translate(0,0)");
+
+    var bar5 = svg5.selectAll("rect")
+        .data(data5)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return x5(d.State_Code); })
+        // return xScale(d.x0+d.x)- (d.x0==0 ? 0 : xScale(d.x0));
+        .attr("y", function (d) { return y5(parseInt(d.Tweets_Cnt)); })
+        .attr("width", x5.bandwidth())
+        .attr("height", function (d) { return y5(0) - y5(parseInt(d.Tweets_Cnt)); })
+        .attr("fill", function (d) { return color5(d.State_Code); })
+        .on("mouseover", mouseover5)
+        .on("mouseout", mouseout5)
+        .attr("id", function (d) { return `rect-${d.State_Code}` });
+
+
+    // svg5.append("g").call(x5_axis).attr("transform", "translate(0,500");
+
+    bar5.exit().remove();
+    y5_desc.exit().remove();
+    title5.exit().remove();
+    x5_axis.exit().remove();
+});
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,197 @@
+// Add your JavaScript code here
+const MAX_WIDTH = Math.max(1080, window.innerWidth);
+const MAX_HEIGHT = 720;
+const margin = { top: 40, right: 100, bottom: 40, left: 100 };
+
+const width = 540;
+const height = 330;
+
+// Assumes the same graph width, height dimensions as the example dashboard. Feel free to change these if you'd like
+let graph_1_width = (MAX_WIDTH / 2) - 10, graph_1_height = 250;
+let graph_2_width = (MAX_WIDTH / 2) - 10, graph_2_height = 275;
+let graph_3_width = MAX_WIDTH / 2, graph_3_height = 575;
+
+// console.log("hello");
+
+// Graph1
+var svg1 = d3.select("#graph1")
+    .append("svg")
+    .attr("width", graph_1_width)
+    .attr("height", 320)
+    .attr("left", 500)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+let countRef = svg1.append("g");
+
+// all valid years
+const all_years = ['1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
+
+// button for year1
+d3.select("#selectButton1")
+    .selectAll('myOptions')
+    .data(all_years)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return parseInt(d); }) // corresponding value returned by the button
+
+// button for year2
+d3.select("#selectButton2")
+    .selectAll('myOptions')
+    .data(all_years)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return parseInt(d); }) // corresponding value returned by the button
+
+let x = d3.scaleLinear()
+    .range([0, width - margin.left - margin.right]);
+
+let y = d3.scaleBand()
+    .range([0, height - margin.top - margin.bottom])
+    .padding(0.1);  // Improves readability
+
+let y_axis_label = svg1.append("g").attr("id", "y1_label");
+
+let color = d3.scaleOrdinal()
+    .range(d3.quantize(d3.interpolateHcl("#66a0e2", "#81c2c3"), 10));
+
+svg1.append("text")
+    .attr("transform", `translate(${(width - margin.left - margin.right) / 2},
+${(height - margin.top - margin.bottom) + 25})`)       // HINT: Place this at the bottom middle edge of the graph - use translate(x, y) that we discussed earlier
+    .style("text-anchor", "middle")
+    .style("font-size", 18)
+    .text("Global Sells Counts(In Millions)");
+
+// TODO: Add y-axis label
+svg1.append("text")
+    .attr("transform", `translate(-150, ${(height - margin.top - margin.bottom) / 2})`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
+    .style("text-anchor", "middle")
+    .style("font-size", 18)
+    .text("Game");
+
+let chart1_title = svg1.append("text");
+function update(year1, year2) {
+    // svg1.selectAll("rect").remove();
+    // svg1.selectAll("text").remove();
+
+    /* properly set the title in the chart of Q1,
+       if only 1 year, return "in 20xx",
+       otherwise, return "from 19xx to 20xx"
+    */
+    var chart1_title_string;
+    if (year1 == year2) {
+        chart1_title_string = " in " + year1;
+    }
+    else {
+        chart1_title_string = " from " + year1 + " to " + year2;
+    }
+
+    // TODO: Add chart title
+    chart1_title
+        .attr("transform", `translate(${(width - margin.left - margin.right) / 2}, ${-10})`)      // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
+        .style("text-anchor", "middle")
+        .style("font-size", 18)
+        .text("Top 10 Best Game Sellers" + chart1_title_string);
+
+    function checkYear(year) {
+        // Keep valid data
+        return year >= year1 && year <= year2;
+    }
+
+    d3.csv("./data/video_games.csv").then(function (data) {
+        var dataFilter = data.filter(function (d) {
+            return checkYear(parseFloat(d.Year));
+        })
+        data = dataFilter.slice(0, 10);
+
+        console.log(year1, year2);
+        console.log(data);
+        let counts = countRef.selectAll("text").data(data);
+
+        x.domain([0, data[0].Global_Sales])
+
+        y.domain(data.map(function (d) { return d.Name }))
+
+        y_axis_label.call(d3.axisLeft(y).tickSize(0).tickPadding(10));
+
+        let bars = svg1.selectAll("rect").data(data);
+
+        color.domain(data.map(function (d) { return d.Name }))
+
+        bars.enter()
+            .append("rect")
+            .on("mouseover", mouseover_barplot)
+            .on("mouseout", mouseout_barplot)
+            .merge(bars)
+            .attr("fill", function (d) { return color(d.Name) }) // Here, we are using functin(d) { ... } to return fill colors based on the data point d
+            .attr("x", x(0))
+            .attr("y", function (d) { return y(d.Name) })               // HINT: Use function(d) { return ...; } to apply styles based on the data point (d)
+            .attr("width", function (d) { return x(d.Global_Sales) })
+            .attr("height", y.bandwidth())
+            .attr("id", function (d) { return `rect-${d.Rank}` });
+
+        counts.enter()
+            .append("text")
+            .merge(counts)
+            .attr("x", function (d) { return x(d.Global_Sales) + 10 })       // HINT: Add a small offset to the right edge of the bar, found by x(d.count)
+            .attr("y", function (d) { return y(d.Name) + 18 })       // HINT: Add a small offset to the top edge of the bar, found by y(d.artist)
+            .style("text-anchor", "start")
+            .text(function (d) { return d.Global_Sales });           // HINT: Get the count of the artist
+
+        bars.exit().remove();
+        counts.exit().remove();
+    });
+}
+
+var g1_s1_year_value = 1980;
+var g1_s2_year_value = 1980;
+
+update(1980, 1980);
+
+d3.select("#selectButton1").on("change", function (d) {
+    // get new value from the select button
+    g1_s1_year_value = d3.select(this).property("value");
+    // run the updateChart function with this selected option
+    if (g1_s1_year_value > g1_s2_year_value) {
+        window.alert("Please select valid time range!");
+    }
+    update(g1_s1_year_value, g1_s2_year_value);
+})
+
+d3.select("#selectButton2").on("change", function (d) {
+    // get new value from the select button
+    g1_s2_year_value = d3.select(this).property("value");
+    // run the updateChart function with this selected option
+    if (g1_s1_year_value > g1_s2_year_value) {
+        window.alert("Please select valid time range!");
+    }
+    update(g1_s1_year_value, g1_s2_year_value);
+})
+/**
+ * Returns a darker shade of a given color
+ */
+function darkenColor(color, percentage) {
+    return d3.hsl(color).darker(percentage);
+}
+
+let mouseover_barplot = function (d) {
+    // console.log("mouseover!");
+    // console.log(d);
+    // console.log(color(d.Global_Sales));
+    svg1.select(`#rect-${d.Rank}`).attr("fill", function (d) {
+        return darkenColor(color(d.Name), 0.5);
+    });
+};
+
+// Restore bar fill to original color on mouseout
+let mouseout_barplot = function (d) {
+    svg1.select(`#rect-${d.Rank}`).attr("fill", function (d) {
+        return color(d.Name)
+    });
+};
+
+
+
