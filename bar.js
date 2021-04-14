201f4c7a8047778329e5e3f3ace9d233af08ef7a
JiayangWu <530081999@qq.com>
2021-04-14 12:31:54 -0400

    finish 2 graphs, start adjust pos

--- bar.js
+++ bar.js
@@ -1,6 +1,6 @@
 
 
-var svg3 = d3.select("#graph5")
+var svg5 = d3.select("#graph5")
     .append("svg")
     .attr("width", 1400)
     .attr("height", 500 * 2)
@@ -8,133 +8,130 @@ var svg3 = d3.select("#graph5")
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
-let countRef3 = svg3.append("g");
+let countRef5 = svg5.append("g");
 
 
-var x3 = d3.scaleBand()
+var x5 = d3.scaleBand()
     .range([0, 1100])
     // .attr("transform", "translate(0,500)")
     .padding(0.5);
 
 var x_axis = d3.axisBottom()
-    .scale(x3);
+    .scale(x5);
 
 // # https://colourco.de
-let color3 = d3.scaleOrdinal()
+let color5 = d3.scaleOrdinal()
     .range(d3.quantize(d3.interpolateHcl("#9AA6DD", "#ded79b"), 51));
 
-let y_labels = svg3.append("g");
+let y5_labels = svg5.append("g");
 
-var y3 = d3.scaleLinear()
+var y5 = d3.scaleLinear()
     .range([500, 0])
 
-// svg3.append("text")
+// svg5.append("text")
 //     .attr("transform", `translate(${(width - margin.left - margin.right) / 2},
 // ${(height - margin.top - margin.bottom) + 60})`)       // HINT: Place this at the bottom middle edge of the graph - use translate(x, y) that we discussed earlier
 //     .style("text-anchor", "middle")
 //     .style("font-size", 18)
 //     .text("Publisher");
 
-let y3_desc = svg3.append("text");
-let x3_desc = svg3.append("text");
-let title3 = svg3.append("text");
+let y5_desc = svg5.append("text");
+let title5 = svg5.append("text");
+let x5_desc = svg5.append("text");
 
-let tooltip3 = d3.select("body")
+let tooltip5 = d3.select("body")
     .append("div")
     .attr("class", "tooltip3")
     .style("opacity", 0);
 
-let x3_publishers = svg3.append("g");
+let x5_axis = svg5.append("g");
 
-let mouseover3 = function (d) {
-    let color_span = `<span style="color: ${color3(d.State_Code)}; font-size: 18px; font-weight:bold;">`;
+let mouseover5 = function (d) {
+    let color_span = `<span style="color: ${color5(d.State_Code)}; font-size: 18px; font-weight:bold;">`;
     let html = `${d.State_Code}<br/>
                 Tweets Count:<br/>
                 ${color_span}${d.Tweets_Cnt}</span>`;
-    // console.log(color3(d.State_Code));
+    // console.log(color5(d.State_Code));
     // console.log(parseInt(d.Tweets_Cnt));
-    // console.log(color3(d.State_Code));
+    // console.log(color5(d.State_Code));
     // Show the tooltip and set the position relative to the event X and Y location
-    tooltip3.html(html)
-        .style("left", `${(d3.event.pageX) + 30}px`)
+    tooltip5.html(html)
+        .style("left", `${(d3.event.pageX) + 50}px`)
         .style("top", `${(d3.event.pageY) - 100}px`)
-        .style("box-shadow", `3px 3px 7px ${color3(d.State_Code)}`)
+        .style("box-shadow", `5px 5px 7px ${color5(d.State_Code)}`)
         .style("background-color", "#ffffff")
         .transition()
         .duration(400)
         .style("opacity", 0.9)
 
-    svg3.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
-        return darkenColor(color3(d.State_Code), 0.5);
+    svg5.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
+        return darkenColor(color5(d.State_Code), 0.5);
     });
 }
 
-let mouseout3 = function (d) {
+let mouseout5 = function (d) {
     // Set opacity back to 0 to hide
-    tooltip3.transition()
-        .duration(300)
+    tooltip5.transition()
+        .duration(500)
         .style("opacity", 0);
-    svg3.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
-        return color3(d.State_Code);
+    svg5.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
+        return color5(d.State_Code);
     });
 };
 
-d3.csv("./data/full_dataset.csv").then(function (data3) {
-    svg3.selectAll("rect").remove();
-    // svg3.selectAll("text").remove();
-    // x.domain([0, data3[0].Global_Sales])
-    x3.domain(data3.map(function (d) { return d.State_Code }));
-    y3.domain([0, d3.max(data3, function (d) { return parseInt(d.Tweets_Cnt) })]);
+d3.csv("./data/full_dataset.csv").then(function (data5) {
+    svg5.selectAll("rect").remove();
 
-    // y-label
-    y3_desc.attr("transform", `translate(-60, ${(height - margin.top - margin.bottom) / 2 + 80})rotate(-90)`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
+    x5.domain(data5.map(function (d) { return d.State_Code }));
+    y5.domain([0, d3.max(data5, function (d) { return parseInt(d.Tweets_Cnt) })]);
+
+    // Add y-axis description
+    y5_desc.attr("transform", `translate(-60, ${(height - margin.top - margin.bottom) / 2 + 80})rotate(-90)`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
         .style("text-anchor", "middle")
         .style("font-size", 18)
         .text("Tweets Counts in Each State");
 
     // Add chart title
-    x3_desc.attr("transform", `translate(500, -25)`)      // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
+    title5.attr("transform", `translate(500, -25)`)      // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
         .style("text-anchor", "middle")
         .style("font-size", 18)
         .text("Tweets Count VS State Code");
 
-    title3.style("font-size", 18)
+    // Add x-axis description
+    x5_desc.style("font-size", 18)
         .text("State Code").attr("transform", `translate(500, 550)`);
 
-    // x-label
-    x3_publishers
-        // .attr("transform", "translate(0, 30)")
+    // Create x-axis
+    x5_axis
         .call(x_axis)
-        // .selectAll("text")
-        // 
         .style("text-anchor", "end")
         .attr("transform", "translate(0,500)");
 
 
-    y_labels.call(d3.axisLeft(y3).tickSizeOuter(0))
+    y5_labels.call(d3.axisLeft(y5).tickSizeOuter(0))
         .attr("transform", "translate(0,0)");
 
-    var bar3 = svg3.selectAll("rect")
-        .data(data3)
+    var bar5 = svg5.selectAll("rect")
+        .data(data5)
         .enter()
         .append("rect")
-        .attr("x", function (d) { return x3(d.State_Code); })
+        .attr("x", function (d) { return x5(d.State_Code); })
         // return xScale(d.x0+d.x)- (d.x0==0 ? 0 : xScale(d.x0));
-        .attr("y", function (d) { return y3(parseInt(d.Tweets_Cnt)); })
-        .attr("width", x3.bandwidth())
-        .attr("height", function (d) { return y3(0) - y3(parseInt(d.Tweets_Cnt)); })
-        .attr("fill", function (d) { return color3(d.State_Code); })
-        .on("mouseover", mouseover3)
-        .on("mouseout", mouseout3)
+        .attr("y", function (d) { return y5(parseInt(d.Tweets_Cnt)); })
+        .attr("width", x5.bandwidth())
+        .attr("height", function (d) { return y5(0) - y5(parseInt(d.Tweets_Cnt)); })
+        .attr("fill", function (d) { return color5(d.State_Code); })
+        .on("mouseover", mouseover5)
+        .on("mouseout", mouseout5)
         .attr("id", function (d) { return `rect-${d.State_Code}` });
 
 
-    // svg3.append("g").call(x3_publishers).attr("transform", "translate(0,500");
+    // svg5.append("g").call(x5_axis).attr("transform", "translate(0,500");
 
-    bar3.exit().remove();
-    y3_desc.exit().remove();
-    x3_desc.exit().remove();
-    x3_publishers.exit().remove();
+    bar5.exit().remove();
+    y5_desc.exit().remove();
+    title5.exit().remove();
+    x5_axis.exit().remove();
 });
 
 
---
+++ graph6.js
@@ -0,0 +1,131 @@
+
+
+var svg6 = d3.select("#graph6")
+    .append("svg")
+    .attr("width", 1400)
+    .attr("height", 500 * 2)
+    // .attr("left", 500)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+var x6 = d3.scaleBand()
+    .range([0, 1100])
+    // .attr("transform", "translate(0,500)")
+    .padding(0.5);
+
+var x6_axis = d3.axisBottom()
+    .scale(x6);
+
+// # https://colourco.de
+let color6 = d3.scaleOrdinal()
+    .range(d3.quantize(d3.interpolateHcl("#ded79b", "#9AA6DD"), 51));
+
+var y6 = d3.scaleLinear()
+    .range([500, 0])
+
+let y6_desc = svg6.append("text");
+let title6 = svg6.append("text");
+let x6_desc = svg6.append("text");
+let y6_labels = svg6.append("g");
+
+let tooltip6 = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip3")
+    .style("opacity", 0);
+
+let x6_axis_g = svg6.append("g");
+
+let mouseover6 = function (d) {
+    let color_span = `<span style="color: ${color6(d.State_Code)}; font-size: 18px; font-weight:bold;">`;
+    let html = `${d.State_Code}<br/>
+            Tweets Count:<br/>
+            ${color_span}${d.Turnout_Rate}</span>`;
+    // console.log(color6(d.State_Code));
+    // console.log(parseFloat(d.Turnout_Rate));
+    // console.log(color6(d.State_Code));
+    // Show the tooltip and set the position relative to the event X and Y location
+    tooltip6.html(html)
+        .style("left", `${(d3.event.pageX) + 50}px`)
+        .style("top", `${(d3.event.pageY) - 100}px`)
+        .style("box-shadow", `5px 5px 7px ${color6(d.State_Code)}`)
+        .style("background-color", "#ffffff")
+        .transition()
+        .duration(400)
+        .style("opacity", 0.9)
+
+    svg6.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
+        return darkenColor(color6(d.State_Code), 0.5);
+    });
+}
+
+let mouseout6 = function (d) {
+    // Set opacity back to 0 to hide
+    tooltip6.transition()
+        .duration(500)
+        .style("opacity", 0);
+    svg6.select(`#rect-${d.State_Code}`).attr("fill", function (d) {
+        return color6(d.State_Code);
+    });
+};
+
+d3.csv("./data/full_dataset.csv").then(function (data) {
+    svg6.selectAll("rect").remove();
+
+    x6.domain(data.map(function (d) { return d.State_Code }));
+    y6.domain([0, d3.max(data, function (d) { return parseFloat(d.Turnout_Rate) })]);
+
+    // Add y-axis description
+    y6_desc.attr("transform", `translate(-60, ${(height - margin.top - margin.bottom) / 2 + 80})rotate(-90)`)       // HINT: Place this at the center left edge of the graph - use translate(x, y) that we discussed earlier
+        .style("text-anchor", "middle")
+        .style("font-size", 18)
+        .text("Tweets Counts in Each State");
+
+    // Add chart title
+    title6.attr("transform", `translate(500, -25)`)      // HINT: Place this at the top middle edge of the graph - use translate(x, y) that we discussed earlier
+        .style("text-anchor", "middle")
+        .style("font-size", 18)
+        .text("Tweets Count VS State Code");
+
+    // Add x-axis description
+    x6_desc.style("font-size", 18)
+        .text("State Code").attr("transform", `translate(500, 550)`);
+
+    // Create x-axis
+    x6_axis_g
+        .call(x6_axis)
+        .style("text-anchor", "end")
+        .attr("transform", "translate(0,500)");
+
+
+    y6_labels.call(d3.axisLeft(y6).tickSizeOuter(0))
+        .attr("transform", "translate(0,0)");
+
+    var bar5 = svg6.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return x6(d.State_Code); })
+        // return xScale(d.x0+d.x)- (d.x0==0 ? 0 : xScale(d.x0));
+        .attr("y", function (d) { return y6(parseFloat(d.Turnout_Rate)); })
+        .attr("width", x6.bandwidth())
+        .attr("height", function (d) { console.log(parseFloat(d.Turnout_Rate)); return y6(0) - y6(parseFloat(d.Turnout_Rate)); })
+        .attr("fill", function (d) { return color6(d.State_Code); })
+        .on("mouseover", mouseover6)
+        .on("mouseout", mouseout6)
+        .attr("id", function (d) { return `rect-${d.State_Code}` });
+
+
+    // svg6.append("g").call(x6_axis).attr("transform", "translate(0,500");
+
+    bar5.exit().remove();
+    y6_desc.exit().remove();
+    title6.exit().remove();
+    x6_axis_g.exit().remove();
+});
+
+
+
+
+
+
