65176d6d832fa43712eac4fd39019fb9cf551ade
JiayangWu <530081999@qq.com>
2021-04-14 23:15:20 -0400

    finished the prototype of map

--- map2.js
+++ map2.js
@@ -20,14 +20,36 @@ let tooltip8 = d3.select("body")
     .style("opacity", 0);
 
 let mouseover8 = function (d) {
+    // let color_span = `<span style="color: ${darkenColor(color6(d.State_Code), 1.1)}; font-size: 18px; font-weight:bold;">`;
+    // let html = `${d.State_Code}<br/>
+    //         Turnout Rate:<br/>
+    //         ${color_span}${d.Turnout_Rate}%</span>`;
+    // console.log(d);
+    // console.log(d.properties.name);
+    let state_name = d.properties.name;
+    let html = `${state_name}`;
     tooltip8.html(html)
         .style("left", `${(d3.event.pageX) + 50}px`)
         .style("top", `${(d3.event.pageY) - 100}px`)
-        .style("box-shadow", `5px 5px 7px ${color6(d.State_Code)}`)
+        .style("box-shadow", `5px 5px 7px #000000`)
         .style("background-color", "#ffffff")
         .transition()
         .duration(400)
         .style("opacity", 0.9)
+    // console.log("dsadas");
+
+    svg8.select(`#rect-${d.id}`).attr("fill", function (d) {
+        return darkenColor("#fc8d62", 0.5);
+    });
+}
+
+let mouseout8 = function (d) {
+    tooltip8.transition()
+        .duration(500)
+        .style("opacity", 0);
+    svg8.select(`#rect-${d.id}`).attr("fill", function (d) {
+        return "#fc8d62";
+    });
 }
 
 d3.json("us-states.json").then((world) => {
@@ -52,13 +74,16 @@ d3.json("us-states.json").then((world) => {
         .data(map_data)
         .enter().append("path")
         .attr("fill", (d) => {
-            return "#fc8d62"; // Orange for others
+            return "#fc8d62"; // Orange 
         })
         .on("mouseover", mouseover8)
+        .on("mouseout", mouseout8)
         .attr("d", d3.geoPath()
             .projection(path)
         )
-        .style("stroke", "#FFFFFF");
+        .attr("id", function (d) { return `rect-${d.id}` })
+        .style("stroke", "#FFFFFF")
+        .attr("transform", "translate(150,90)");
     // .style("stroke", "#FFFFFF");
 
     // svg8.append("path")
