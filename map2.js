b2d8322dc3e4576087f2b4944cb411f2c0597d04
JiayangWu <530081999@qq.com>
2021-04-17 17:35:32 -0400

    apply patterns

--- map2.js
+++ map2.js
@@ -3,112 +3,160 @@ var svg8 = d3.select("#graph8")
     .attr("width", 1400)
     .attr("height", 500 * 2)
     .append("g");
+// .attr("tr")
 
-// var path = d3.geoNaturalEarth()
-//     .scale(width / 1.3 / Math.PI)
-//     .translate([width / 2, height / 2]);
 var path = d3.geoAlbersUsa();
-// console.log("dsadsadas");
-// var projection = d3.geoNaturalEarth()
-//     .scale(width / 1.3 / Math.PI)
-//     .translate([width / 2, height / 2])
-// var path = d3.geoPath().projection(d3.geoMercator());
-// console.log("Dasdasdas");
+
 let tooltip8 = d3.select("body")
     .append("div")
     .attr("class", "tooltip3")
     .style("opacity", 0);
 
-let mouseover8 = function (d) {
-    // let color_span = `<span style="color: ${darkenColor(color6(d.State_Code), 1.1)}; font-size: 18px; font-weight:bold;">`;
-    // let html = `${d.State_Code}<br/>
-    //         Turnout Rate:<br/>
-    //         ${color_span}${d.Turnout_Rate}%</span>`;
-    // console.log(d);
-    // console.log(d.properties.name);
-    let state_name = d.properties.name;
-    let html = `${state_name}`;
-    tooltip8.html(html)
-        .style("left", `${(d3.event.pageX) + 50}px`)
-        .style("top", `${(d3.event.pageY) - 100}px`)
-        .style("box-shadow", `5px 5px 7px #000000`)
-        .style("background-color", "#ffffff")
-        .transition()
-        .duration(400)
-        .style("opacity", 0.9)
-    // console.log("dsadas");
 
-    svg8.select(`#rect-${d.id}`).attr("fill", function (d) {
-        return darkenColor("#fc8d62", 0.5);
+
+d3.csv("./data/chi2.csv").then(function (data) {
+
+    var state2Winners = {};
+    data.forEach(element => {
+        // console.log({"Tweets_winner": element.Tweets_Winner, "Real_winner": element.Election_Winner });
+        state2Winners[element.State_Name] = { "Tweets_winner": element.Tweets_Winner, "Real_winner": element.Election_Winner };
     });
-}
 
-let mouseout8 = function (d) {
-    tooltip8.transition()
-        .duration(500)
-        .style("opacity", 0);
-    svg8.select(`#rect-${d.id}`).attr("fill", function (d) {
-        return "#fc8d62";
+    d3.json("../us-states.json").then((us) => {
+        console.log(state2Winners);
+        let mouseover8 = function (d) {
+            let state_name = d.properties.name;
+            let html = `${state_name}`;
+            tooltip8.html(html)
+                .style("left", `${(d3.event.pageX) + 50}px`)
+                .style("top", `${(d3.event.pageY) - 100}px`)
+                .style("box-shadow", `5px 5px 7px ${color_map(d)}`)
+                .style("background-color", "#ffffff")
+                .transition()
+                .duration(400)
+                .style("opacity", 0.9)
+
+            // svg8.select(`#rect-${d.id}`).attr("fill", function (d) {
+            //     return darkenColor(color_map(d), 0.5);
+            // });
+        }
+
+        let mouseout8 = function (d) {
+            tooltip8.transition()
+                .duration(500)
+                .style("opacity", 0);
+            svg8.select(`#rect-${d.id}`).attr("fill", function (d) {
+                return color_map(d);
+            });
+        }
+
+        let color_map = function (d) {
+
+            var t_bb = textures.lines() // Tweets: Biden, election: Biden
+                .orientation("diagonal")
+                .size(40)
+                .strokeWidth(26)
+                .stroke("#2b4a93")
+                .background("white");
+
+            // Tweets: Trump, election: Biden
+            var t_tb = textures.circles()
+                .heavier()
+                // .stroke("#FFFFFF")
+                // .fill("#FFFFFF")
+                .background("#2b4a93");
+
+            // Tweets: Biden, election: Trump
+            var t_bt = textures.lines()
+                .orientation("diagonal")
+                .size(40)
+                .strokeWidth(26)
+                .stroke("#C13739")
+                .background("white");
+
+            // Tweets: Trump, election: Trump
+            var t_tt = textures.circles()
+                .heavier()
+                .background("#C13739");
+
+            svg8.call(t_bb);
+            svg8.call(t_tb);
+            svg8.call(t_tt);
+            svg8.call(t_bt);
+            if (d.id < "60") {
+                let state_name = d.properties.name;
+                // console.log(state_name, state2Winners[state_name]["Real_winner"], state2Winners[state_name]["Tweets_Winner"]);
+                if (state2Winners[state_name]["Real_winner"] == "Biden" &&
+                    state2Winners[state_name]["Tweets_winner"] == "Biden") {
+                    // console.log(state_name, t_bb.url);
+                    return t_bb.url(); // blue
+                }
+                else if (state2Winners[state_name]["Real_winner"] == "Biden" &&
+                    state2Winners[state_name]["Tweets_winner"] == "Trump") {
+                    return t_tb.url();
+                }
+                else if (state2Winners[state_name]["Real_winner"] == "Trump" &&
+                    state2Winners[state_name]["Tweets_winner"] == "Biden") {
+                    return t_bt.url();
+                }
+                else {
+                    return t_tt.url(); // red
+                }
+            }
+
+        }
+        // console.log(topojson.feature(us, us.objects.states).features);
+
+
+
+
+
+
+        var map_data = topojson.feature(us, us.objects.states).features;
+
+
+
+
+        svg8.append("g")
+            .selectAll("path")
+            .data(map_data)
+            .enter().append("path")
+            .attr("fill", color_map)
+            .on("mouseover", mouseover8)
+            .on("mouseout", mouseout8)
+            .attr("d", d3.geoPath()
+                .projection(path)
+            )
+            .attr("id", function (d) { return `rect-${d.id}` })
+            .style("stroke", "#FFFFFF")
+            .attr("transform", "translate(100, 20)");
+
+        svg8.append("circle")
+            .attr("cx", 360)
+            .attr("cy", 548)
+            .attr("r", 6)
+            .style("fill", "#2b4a93")
+
+        svg8
+            .append("circle")
+            .attr("cx", 360)
+            .attr("cy", 578)
+            .attr("r", 6)
+            .style("fill", "#C13739")
+
+        svg8.append("text")
+            .attr("x", 390)
+            .attr("y", 550)
+            .text("States where Biden is the election winner")
+            .style("font-size", "15px")
+            .attr("alignment-baseline", "middle")
+
+        svg8.append("text")
+            .attr("x", 390)
+            .attr("y", 580)
+            .text("States where Trump is the election winner")
+            .style("font-size", "15px")
+            .attr("alignment-baseline", "middle")
     });
-}
-
-d3.json("../us-states.json").then((us) => {
-
-    // console.log(topojson.feature(us, us.objects.states).features);
-    var map_data = topojson.feature(us, us.objects.states).features;
-
-
-    console.log(map_data);
-
-    svg8.append("g")
-        .selectAll("path")
-        .data(map_data)
-        .enter().append("path")
-        .attr("fill", (d) => {
-            return "#fc8d62"; // Orange 
-        })
-        .on("mouseover", mouseover8)
-        .on("mouseout", mouseout8)
-        .attr("d", d3.geoPath()
-            .projection(path)
-        )
-        .attr("id", function (d) { return `rect-${d.id}` })
-        .style("stroke", "#FFFFFF")
-        .attr("transform", "translate(150,90)");
-    // .style("stroke", "#FFFFFF");
-
-    // svg8.append("path")
-    //     .datum(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; }))
-    //     .attr("class", "states")
-    //     .attr("d", path);
-
-    // // .attr("transform", (-1000, 0));/
-    // // .attr("left", -400);
-
-    // svg4.append("circle")
-    //     .attr("cx", 60)
-    //     .attr("cy", 330)
-    //     .attr("r", 6)
-    //     .style("fill", "#fc8d62")
-
-    // svg4
-    //     .append("circle")
-    //     .attr("cx", 60)
-    //     .attr("cy", 360)
-    //     .attr("r", 6)
-    //     .style("fill", "#a6d854")
-
-    // svg4.append("text")
-    //     .attr("x", 80)
-    //     .attr("y", 330)
-    //     .text("Regions where Action is the most popular genre(NA, EU and Other)")
-    //     .style("font-size", "15px")
-    //     .attr("alignment-baseline", "middle")
-
-    // svg4.append("text")
-    //     .attr("x", 80)
-    //     .attr("y", 360)
-    //     .text("Regions where RPG is the most popular genre(Only JP)")
-    //     .style("font-size", "15px")
-    //     .attr("alignment-baseline", "middle")
 });
+
